155bda5c1cb86265298091987e058ae4e594f755
Simon Lydell <simon.lydell@gmail.com>
2019-01-15 21:23:55 +0100

    Parse URLs the same way Next.js does
    ... by using the standard Node.js module 'url'. Next.js already uses it
    itself, so `require`ing it does not blow up the bundle size or anything
    (verified using next-bundle-analyzer). This fixes two things:
    
    - Query parameters are URL decoded.
    - If the same parameter is specified multiple times its value becomes an
    array of all the values.
    
    This aligns the query parameter handling with how it works with vanilla
    Next.js.

--- src/utils/queryString.js
+++ src/utils/queryString.js
@@ -6,19 +6,3 @@ export const toString = (params, prefix = '?') => {
 
   return queryString ? prefix + queryString : ''
 }
-
-export const fromString = str =>
-  str
-    .replace(/^\?/, '')
-    .split('&')
-    .map(str => str.split('='))
-    .filter(([k]) => !!k)
-    .reduce((acc, [k, v]) => Object.assign(acc, { [k]: v }), {})
-
-export const parseUrl = url => {
-    const [pathname, search = ''] = url.split('?')
-    return {
-        pathname,
-        queryParams: fromString(search)
-    }
-}
